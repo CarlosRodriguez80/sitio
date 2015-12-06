f77b45fb4c04443c940974edea013618909e20dc
CarlosRodriguez80 <carlosrodriguez.upe@gmail.com>
2015-12-06 18:47:57 -0200

    ultimo
    fin

--- sitio/settings.py
+++ sitio/settings.py
@@ -116,4 +116,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'carlosrodriguez.upe@gmail.com'
-EMAIL_HOST_PASSWORD = 'kurt1803cobain'
+EMAIL_HOST_PASSWORD = 'pass'
