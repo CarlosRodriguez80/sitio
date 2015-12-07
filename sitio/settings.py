37823ab83e4cd41c8b89e0085f73274f37220362
FernandezSergio <Sergio3886@gmail.com>
2015-12-07 20:44:40 -0300

    Update settings.py

--- sitio/settings.py
+++ sitio/settings.py
@@ -116,5 +116,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sergio3886@gmail.com'
-EMAIL_HOST_PASSWORD = 'Bro338866'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
